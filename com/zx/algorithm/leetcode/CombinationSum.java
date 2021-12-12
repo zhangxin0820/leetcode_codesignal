0e52c676979e309ab9127816ea33307492b2f94c
zhangxin86 <zhangxin86@58.com>
2021-12-12 09:29:16 +0800

    update

--- com/zx/algorithm/leetcode/CombinationSum.java
+++ com/zx/algorithm/leetcode/CombinationSum.java
@@ -1,6 +1,46 @@
-package com.zx.algorithm.leetcode;/**
+package com.zx.algorithm.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
  * Created by zhangxin on 2021/12/05.
  * Time : 09:04
+ * 给定一个无重复元素的正整数数组candidates和一个正整数target,找出candidates中所有可以使数字和为目标数target的唯一组合.
+ * 排序才能使用剪枝的原因：乱序的时候，target - candidates[i] < 0，但是下一层可能会出现target - candidates[i+1] > 0情况。
  */
 public class CombinationSum {
+    List<List<Integer>> res = new ArrayList<>();
+
+    public static void main(String[] args) {
+        CombinationSum combinationSum = new CombinationSum();
+        int[] candidates = new int[]{2, 3, 6, 7};
+        int target = 7;
+        System.out.println(combinationSum.combinationSum(candidates, target));
+    }
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        int n = candidates.length;
+        Arrays.sort(candidates);
+        ArrayList<Integer> tmp = new ArrayList<>();
+        dfs(candidates, target, n, 0, tmp);
+        return res;
+    }
+
+    private void dfs(int[] candidates, int target, int n, int start, ArrayList<Integer> tmp) {
+        if (target < 0) {
+            return;
+        }
+        if (target == 0) {
+            res.add(new ArrayList<>(tmp));
+            return;
+        }
+        for (int i = start; i < n; i++) {
+            if (target - candidates[i] < 0) break; // 这里是剪枝 前提是必须要从小到大排序数组
+            tmp.add(candidates[i]);
+            dfs(candidates, target-candidates[i], n, i, tmp);
+            tmp.remove(tmp.size() - 1);
+        }
+    }
 }
--- com/zx/algorithm/leetcode/CombinationSum2.java
+++ com/zx/algorithm/leetcode/CombinationSum2.java
@@ -1,6 +1,51 @@
-package com.zx.algorithm.leetcode;/**
+package com.zx.algorithm.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
  * Created by zhangxin on 2021/12/05.
  * Time : 17:21
+ * 给定一个无重复元素的正整数数组candidates和一个正整数target,找出candidates中所有可以使数字和为目标数target的唯一组合.
+ * candidates中的每个数字在每个组合中只能使用一次.
+ *
+ * 去重：如果i > start && candidates[i] == candidates[i-1]，直接跳过这次循环，保证同样的元素在tmp中的同一个位置只被选择一次，这里有点难理解。
+ * 举个例子，例如原数组经过排序为[1,1,2,5],target为8，如果不这样做，两个1都会被选择放在第一个位置，然后进递归，这样就会有两个[1,2,5]被加入结果集，导致重复。
  */
 public class CombinationSum2 {
+
+    List<List<Integer>> res = new ArrayList<>();
+
+    public static void main(String[] args) {
+        CombinationSum2 combinationSum = new CombinationSum2();
+        int[] candidates = new int[]{2, 3, 6, 7};
+        int target = 7;
+        System.out.println(combinationSum.combinationSum2(candidates, target));
+    }
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        int n = candidates.length;
+        ArrayList<Integer> tmp = new ArrayList<>();
+        Arrays.sort(candidates);
+        dfs(candidates, target, n, 0, tmp);
+        return res;
+    }
+
+    private void dfs(int[] candidates, int target, int n, int start, ArrayList<Integer> tmp) {
+        if (target < 0) {
+            return;
+        }
+        if (target == 0) {
+            res.add(new ArrayList<>(tmp));
+            return;
+        }
+        for (int i = start; i < n; i++) {
+            if (target - candidates[i] < 0) break;
+            if (i > start && candidates[i] == candidates[i-1]) continue;
+            tmp.add(candidates[i]);
+            dfs(candidates, target-candidates[i], n, i+1, tmp);
+            tmp.remove(tmp.size() - 1);
+        }
+    }
 }
